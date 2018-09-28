1854ff27b879a81f381be77d43a390b1a727b073
nick <xiuyi8023@126.com>
2018-09-28 18:29:52 +0800

    修改RXBus方法，与Context绑定

--- BaseLibrary/app/src/main/java/com/chen/baselibrary/util/RxBus.java
+++ BaseLibrary/app/src/main/java/com/chen/baselibrary/util/RxBus.java
@@ -1,5 +1,11 @@
 package com.chen.baselibrary.util;
 
+import android.app.Instrumentation;
+import android.content.Context;
+
+import com.orhanobut.logger.Logger;
+
+import java.util.HashMap;
 import java.util.Map;
 
 import io.reactivex.Observable;
@@ -21,11 +27,14 @@ import io.reactivex.subjects.Subject;
 public class RxBus {
     private volatile static RxBus mDefaultInstance;
     private final Subject<Object> mBus;
-    private CompositeDisposable compositeDisposable;
+    /**
+     * 将订阅解绑绑定到Context，调用unSubscribe方法只解绑指定的Content的订阅
+     */
+    private Map<Context,CompositeDisposable> cxtDisposaleMap;
 
     private RxBus() {
         mBus = PublishSubject.create().toSerialized();
-        compositeDisposable = new CompositeDisposable();
+        cxtDisposaleMap = new HashMap<>();
     }
 
     public static RxBus getInstance() {
@@ -48,17 +57,26 @@ public class RxBus {
 
     /**
      * 订阅事件
+     * @param context 绑定上下文
      * @param eventType 事件类型
      * @param consumer 事件消费者
      * @param <T> 事件类型
      */
-    public <T> void subscribe(final Class<T> eventType,Consumer<T> consumer){
+    public <T> void subscribe(Context context,final Class<T> eventType,Consumer<T> consumer){
         mBus.ofType(eventType).subscribe(consumer, throwable -> {
             // onError
         }, () -> {
             // onComplete
         }, disposable -> {
             // onSubscribe
+            CompositeDisposable compositeDisposable;
+            if(cxtDisposaleMap.containsKey(context)){
+                compositeDisposable = cxtDisposaleMap.get(context);
+            }else{
+                compositeDisposable = new CompositeDisposable();
+                cxtDisposaleMap.put(context,compositeDisposable);
+            }
+
             compositeDisposable.add(disposable);
         });
     }
@@ -66,7 +84,24 @@ public class RxBus {
     /**
      * 解除订阅，否则会造成内存溢出
      */
-    public void unSubscribe(){
-        compositeDisposable.clear();
+    public void unSubscribe(Context context){
+        if(cxtDisposaleMap.containsKey(context)){
+            CompositeDisposable compositeDisposable = cxtDisposaleMap.get(context);
+            compositeDisposable.clear();
+        }
+
+        Logger.e("RxBus订阅对象列表数量:%d" + getSubscribeSize());
+    }
+
+    /**
+     * 获取订阅的对象数量
+     * @return
+     */
+    private int getSubscribeSize(){
+        int i = 0;
+        for(Map.Entry<Context,CompositeDisposable> entry:cxtDisposaleMap.entrySet()){
+            i += entry.getValue().size();
+        }
+        return i;
     }
 }
